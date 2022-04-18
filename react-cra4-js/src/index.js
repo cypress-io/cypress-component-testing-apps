a1eec960722d306ffabdfc41c530139e245a1411
Ely Lucas <ely@meta-tek.net>
2022-04-18 16:13:09 -0600

    downgrading react-cra4-js to react 17

--- react-cra4-js/src/index.js
+++ react-cra4-js/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { worker } from './mocks';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // Start a mock API server to handle auth requests
