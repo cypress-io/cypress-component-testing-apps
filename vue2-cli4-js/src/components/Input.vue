7610c3915930bdbcd30358d1c0af536e6283d7e2
Ely Lucas <ely@meta-tek.net>
2022-04-19 14:09:20 -0600

    vue2-cli4-js setup

---
+++ vue2-cli4-js/src/components/Input.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    class="p-2 text-gray-500 focus:outline-none border-2"
+    :value="model"
+    @input="handleChange"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    model: String,
+  },
+  methods: {
+    handleChange(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+};
+</script>
