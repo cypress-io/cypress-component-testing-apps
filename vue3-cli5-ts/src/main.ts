fd4371d0d6b992df62300afa5d5734778d94aa68
Ely Lucas <ely@meta-tek.net>
2022-04-18 18:29:42 -0600

    removing cypress from vue3-cli5-ts

--- vue3-cli5-ts/src/main.ts
+++ vue3-cli5-ts/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/tailwind.css'
+import './assets/main.css'
 import { worker } from './mocks';
 
 // Start a mock API server to handle auth requests
