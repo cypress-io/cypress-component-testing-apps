bebfb77613bee2e6ced70bd1df8e9e34944c993d
AtofStryker <bglesias@gmail.com>
2024-11-27 12:27:43 -0500

    chore: update vite to vite 6. svelte-vite project needs to wait until we support svelte 5

--- react-vite-ts/cypress/support/component.ts
+++ react-vite-ts/cypress/support/component.ts
@@ -19,7 +19,7 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-import { mount } from 'cypress/react18'
+import { mount } from 'cypress/react'
 // Ensure global app styles are loaded:
 import '../../src/index.css';
 
