1697556e4ce61d0908dbe9a1c255356de0a1cc75
Bill Glesias <bglesias@gmail.com>
2024-12-02 16:09:18 -0500

    chore: update angular-standalone to v19 (need prebuilt binary to work) (#38)

--- angular-standalone/src/app/button/button.component.ts
+++ angular-standalone/src/app/button/button.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
-  standalone: true,
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
