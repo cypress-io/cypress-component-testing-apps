d03b20477da9d0f88e9c390cdba5fb033703af09
Bill Glesias <bglesias@gmail.com>
2025-01-08 10:03:47 -0500

    chore: merge main into release 14 (#42)
    * chore: update react-webpack5-js app (#36)
    
    * update cypress to use 14 prerelease

--- react-next14-ts/cypress/support/component.ts
+++ react-next14-ts/cypress/support/component.ts
@@ -19,7 +19,7 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-import { mount } from 'cypress/react18'
+import { mount } from 'cypress/react'
 
 //Ensure global styles are loaded
 import '../../src/styles/globals.css';
