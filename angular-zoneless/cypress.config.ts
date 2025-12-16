ddce6f1a4aa7744b3ab29a39d067d9cb6557d0dd
Bill Glesias <bglesias@gmail.com>
2025-12-16 15:55:42 -0500

    chore: configure e2e testing

--- angular-zoneless/cypress.config.ts
+++ angular-zoneless/cypress.config.ts
@@ -8,4 +8,10 @@ export default defineConfig({
     },
     specPattern: "**/*.cy.ts",
   },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
